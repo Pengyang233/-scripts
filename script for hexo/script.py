8a23f624cc2989d2bd2b470b6524f396391ec92b
Pengyang233 <39028239+Pengyang233@users.noreply.github.com>
2020-07-26 17:35:46 +0800

    update script.py
    添加 同一性检查

--- script for hexo/script.py
+++ script for hexo/script.py
@@ -6,12 +6,34 @@ import datetime
 import re
 import filecmp
 
-NOTE_ROOT = 'Notes' #笔记文件夹
-SRC_ROOT = 'wiki-source' # 博客的源文件
+NOTE_ROOT = 'Notes'
+SRC_ROOT = 'wiki-source'
 
 def mkdir_if_missing(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+def check_two_folders():
+    #检查两个路径下是否存在不同的文件
+    # 不会发生 copy行为，只会删除 wiki_path 中的差异文件
+    # 往往在更新 note_path 中的笔记组织形式的时候，调用此函数删除wiki path中的老笔记和 asset，后面调用 add_head_and_copy() 来移动新的文件
+    wiki_path_prefix = os.path.join(os.path.join(SRC_ROOT, 'source', '_posts')) # SRC_ROOT//source//_posts
+    note_path_prefix = NOTE_ROOT
+    for root, folders, files in os.walk(wiki_path_prefix):
+        for cur_wiki_name in files:
+            if os.path.splitext(cur_wiki_name)[-1] == '.md':
+                cur_wiki_path = os.path.join(root, cur_wiki_name)
+                cur_note_path = cur_wiki_path.replace(wiki_path_prefix, note_path_prefix)
+                if not os.path.exists(cur_note_path):
+                    os.remove(cur_wiki_path)
+                    #检查wiki path下是不是有assst文件夹
+                    wiki_asset_path = os.path.join(root, cur_wiki_name.replace('.md', ''))
+                    if os.path.exists(wiki_asset_path):
+                        pass
+                        os.remove(wiki_asset_path)
+            #删除空的文件夹
+        if len(os.listdir(root))==0:
+            os.rmdir(root)
 
 def add_head_and_copy(note_path, wiki_path):
     # ---
@@ -25,11 +47,12 @@ def add_head_and_copy(note_path, wiki_path):
         raise Exception('File: {} is not .md file'.format(path))
     else:
         # 判定分类情况，若直接在 Notes目录下，则认为是待分类
-        # 注意不同系统下的地址分隔符，默认Notes下第一个子文件夹为分类
+        # 注意地址分隔符，默认Notes下第一个子文件夹为分类(未采用)
         if os.path.splitext(note_path.split('\\')[1])[-1]=='.md':
             cate = ['待分类']
-        else:
-            cate = note_path.split('\\')[1]
+        else:   #如果指定了的话好像不能实现多级目录分类了
+            # cate = note_path.split('\\')[1]
+            cate = ''
         with open(note_path, 'r', encoding='utf-8') as f:
             content = f.readlines()
         # 替换图片的路径
@@ -50,6 +73,7 @@ def add_head_and_copy(note_path, wiki_path):
             f.writelines(content)
 
 def move_img(note_path, wiki_path):
+    #输入的是 note path
     note_name = os.path.split(note_path)[-1].split('.')[0]
     img_folder = os.path.join(os.path.split(note_path)[0], '_asset',note_name)
     if os.path.exists(img_folder):
@@ -66,6 +90,7 @@ def same_check(note_path):
     return (diff > 7)
 
 def main():
+    check_two_folders()
     for root,folder,files in os.walk(NOTE_ROOT):
         for f in files:
             if os.path.splitext(f)[-1]=='.md':
@@ -77,10 +102,10 @@ def main():
                     mkdir_if_missing(os.path.split(wiki_path)[0])
                     add_head_and_copy(note_path, wiki_path)
                     move_img(note_path, wiki_path)
-                    # 输出信息
-                    # note_name = os.path.splitext(note_path.split('\\')[-1])[0]
-                    # print('Update note: {}'.format(note_name))
-    # print('== Done ==')
+
+                    note_name = os.path.splitext(note_path.split('\\')[-1])[0]
+                    print('Update note: {}'.format(note_name))
+    print('== Done ==')
     os.chdir(SRC_ROOT)
     os.system("git add .")
     os.system("git commit -m 'update'")
